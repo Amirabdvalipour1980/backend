bdc5be5c9c3a52b2c993205362a0c94fde54f34a
Amirabdvalipour1980 <amirabdevalipour@gmail.com>
2025-11-06 00:22:58 -0800

    21

--- index.js
+++ index.js
@@ -15,4 +15,57 @@ app.get('/', (req, res) => {
    });
 })
 
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Website</title>
+    <link rel="stylesheet" href="/css/home.css">
+    <link rel="stylesheet" href="/css/layout.css">
+    <link rel="stylesheet" href="fontawesome-free-6.6.0-web/css/all.css">
+</head>
+<body>
+    <nav>
+        <div class="links">
+            <a href="#">
+                <i class="fa fa-home"></i>
+                صفحه اصلی
+            </a>
+            <a href="#">
+                <i class="fa fa-newspaper"></i>
+                آخرین اخبار
+            </a>
+            <a href="#">
+                <i class="fa fa-headphones"></i>
+                تماس با ما
+            </a>
+            <a href="#">
+                <i class="fa fa-phone"></i>
+                درباره ما
+            </a>
+        </div>
+        <div class="searchbar">
+            <i class="fa fa-search"></i>
+            <input type="text" placeholder="جستجو...">
+        </div>
+        <div class="login-register">
+            <a href="/login">
+                ورود / ثبت نام
+                <i class="fa fa-user"></i>
+            </a>
+        </div>
+    </nav>
+    <h1>به سایت من خوش آمدید</h1>
+    <p>
+        من 
+        <%= name %> 
+        هستم و 
+        <%= age %>
+        سالمه!!!
+    </p>
+    <a href="/login">ورود به حساب کاربری</a>
+</body>
+</html>
 app.listen(3000);
