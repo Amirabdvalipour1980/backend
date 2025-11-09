4e27ed18afb9be17a2f4f5fa6cfbec11358f6553
Amirabdvalipour1980 <amirabdevalipour@gmail.com>
2025-11-09 00:47:10 -0800

    1111

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+const fs = require('fs');
 const http = require('http');
 const https = require('https');
 const express = require('express');
 const path = require('path');
 const app = express();
 
+var cert=fs.readFileSync('ssl/server.crt','utf8')
+var key=fs.readFileSync('ssl/server.key','utf8')
+var credit={key,cert}
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
@@ -32,6 +36,6 @@ app.get('/user-login', (req, res) => {
    }
 })
 var httpserver=http.createServer(app)
-var httpsserver=https.createServer(app)
+var httpsserver=https.createServer(credit,app)
 httpserver.listen(3000)
-httpsserver.listen(8443)
+httpsserver.listen(443)
