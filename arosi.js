31a8eab1900f0d24674fd6dd079456df90467c8a
Amirabdvalipour1980 <amirabdevalipour@gmail.com>
2025-11-09 09:19:30 -0800

    Update arosi.js

--- arosi.js
+++ arosi.js
@@ -17,7 +17,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
     var name = 'hossein'
-   res.render('home', {
+   res.render('arosi', {
       name
    });
 })
